e3af38ec18a1280ddd680b9b8f6952e63f7d6a27
klc-pakize <pa3kize@gmail.com>
2023-02-03 12:36:26 +0300

    feat: added product models

--- django_admin_panel_custom/products/models.py
+++ django_admin_panel_custom/products/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
 # Create your models here.
+
+class Product(models.Model):
+
+    name = models.CharField(max_length = 100)
+    description = models.TextField(blank = True, null = True)
+    created_date = models.DateTimeField(auto_now_add = True)
+    update_date = models.DateTimeField(auto_now = True)
+    is_in_stock = models.BooleanField(default = True)
+    slug = models.SlugField(null = True, blank = True)  # When we shoot a specific object, the object is captured with a unique value such as the name of that object. We have always captured it with the id until today. Slug is also a bit of text that appears after your domain name in the URL of a page. 
+
+    class Meta:
+        verbose_name = 'Product'
+        verbose_name_plural = 'Products'
+
+    def __str__(self):
+        return self.name
+    
