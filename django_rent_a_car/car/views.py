bc1386b5333e27da88263899370d48f2cc150ab2
klc-pakize <pa3kize@gmail.com>
2023-02-15 13:40:16 +0300

    feat: added car views

--- django_rent_a_car/car/views.py
+++ django_rent_a_car/car/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
 
+from rest_framework.viewsets import ModelViewSet
+
+from .models import Car, Reservation
+from .serializers import CarSerializer
+
 # Create your views here.
+
+class CarView(ModelViewSet):
+
+    queryset = Car.objects.all()
+    serializer_class = CarSerializer
