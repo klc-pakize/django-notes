e94e29307392ee434572186b23327a28153f2a7d
klc-pakize <pa3kize@gmail.com>
2023-02-15 13:50:54 +0300

    feat: created permissions

--- django_rent_a_car/car/views.py
+++ django_rent_a_car/car/views.py
@@ -4,6 +4,7 @@ from rest_framework.viewsets import ModelViewSet
 
 from .models import Car, Reservation
 from .serializers import CarSerializer
+from .permissions import IsStaffOrReadOnly
 
 # Create your views here.
 
@@ -11,3 +12,4 @@ class CarView(ModelViewSet):
 
     queryset = Car.objects.all()
     serializer_class = CarSerializer
+    permission_classes = [IsStaffOrReadOnly]
