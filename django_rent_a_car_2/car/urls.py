0878b283e94b9b26519d3db7dffad027f35a8986
klc-pakize <pa3kize@gmail.com>
2023-02-16 23:55:56 +0300

    feat: created date non-conflict method

--- django_rent_a_car_2/car/urls.py
+++ django_rent_a_car_2/car/urls.py
@@ -2,7 +2,7 @@ from django.urls import path, include
 
 from rest_framework import routers
 
-from .views import CarView, ReservationView
+from .views import CarView, ReservationView, ReservationDetailView
 
 router = routers.DefaultRouter()
 router.register('car', CarView)
@@ -10,4 +10,5 @@ router.register('car', CarView)
 urlpatterns = [
     path("",include(router.urls)),
     path('reservation/', ReservationView.as_view()),
+    path('reservation/<int:pk>/', ReservationDetailView.as_view()),
 ]
--- django_rent_a_car_2/car/views.py
+++ django_rent_a_car_2/car/views.py
@@ -4,6 +4,7 @@ from django.db.models import Q
 from rest_framework.viewsets import ModelViewSet
 from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
 
 from .models import Car, Reservation
 from .serializers import CarSerializer, ReservationSerializer
@@ -27,11 +28,13 @@ class CarView(ModelViewSet):
         else:
             queryset = super().get_queryset().filter(availability = True)
         
+        
         #! We do a detailed filtering with query_params:
         start = self.request.query_params.get('start')
         print(start)
         end = self.request.query_params.get('end')
         print(end)
+
 
         #! Method to list cars that have no reservations between the dates entered by the user
         con1 = Q(start_date__lt = end)
@@ -54,3 +57,32 @@ class ReservationView(ListCreateAPIView):
         if self.request.user.is_staff:
             return super().get_queryset()
         return super().get_queryset().filter(customer = self.request.user)
+
+
+class ReservationDetailView(RetrieveUpdateDestroyAPIView):
+    queryset = Reservation.objects.all()
+    serializer_class = ReservationSerializer
+    
+    #! The user may want to extend the reservation date, but if there is another reservation for the same vehicle on the desired dates, she should receive a warning:
+
+    def update(self, request, *args, **kwargs):
+        partial = kwargs.pop('partial', False)
+        instance = self.get_object()  # Gets which object it is from here
+        serializer = self.get_serializer(instance, data=request.data, partial=partial)
+        serializer.is_valid(raise_exception=True)  # raise_exception = True if serializer passes validation false if false
+
+        end = serializer.validated_data.get('end_date')  # With Valided_data, we reach the data that has passed the validation. Since we wanted to extend the end date after we reached the field we will use, we took the end_date from there.
+        car = serializer.validated_data.get('car')
+        start = instance.start_date  # I was able to access the start_date field like end and car fields, but since they can use both put and patch on the front end, we accessed the object from the variable I assigned above. In Put operation we have to resend all the data, in Patch we can only send the data we want to change.
+        if Reservation.objects.filter(car = car).exists():  # I check the reservation table for any information about the vehicle I want to extend the expiry date. Returns True if present, False otherwise.
+            for res in Reservation.objects.filter(car = car):
+                if res.start_date < end:
+                    return Response({'message':'Car is not available'})
+        return super().update(request, *args, **kwargs)        
+        
+        self.perform_update(serializer)
+
+        return Response(serializer.data)
+
+    def perform_update(self, serializer):
+        serializer.save()
