8aedbfd0fbc49e88a73a410733644da3d9f0d5e7
klc-pakize <pa3kize@gmail.com>
2023-02-20 18:43:02 +0300

    feat: created category view

--- django_stock_app/stock/views.py
+++ django_stock_app/stock/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
 
+from .models import Category, Product, Brand, Firm, Purchases, Sales
+from .serializers import CategorySerializer
+
+from rest_framework.viewsets import ModelViewSet
 # Create your views here.
+
+class CategoryView(ModelViewSet):
+
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
