f60ec04456b6d25004fdb04d20780037cc2a66da
klc-pakize <pa3kize@gmail.com>
2023-02-20 20:40:47 +0300

    feat: added django model permissions

--- django_stock_app/stock/views.py
+++ django_stock_app/stock/views.py
@@ -7,6 +7,7 @@ from .serializers import CategorySerializer, CategoryProductSerializer
 
 from rest_framework.viewsets import ModelViewSet
 from rest_framework import filters
+from rest_framework.permissions import DjangoModelPermissions
 # Create your views here.
 
 class CategoryView(ModelViewSet):
@@ -21,8 +22,19 @@ class CategoryView(ModelViewSet):
     #! Adding the search ('name=) filter attribute to the category endpoint:
     filterset_fields = ['name']
 
+    """
+    Using Django Model Permissions for Stock App Project:
+    1- Create a new user from the admin panel.
+    2- Create a group named Products Manages from the Groups section
+    3- Allow view, delete, change and add operations for Brand, firm, category and product in the group.
+    """
+    #! Note: The get operation is created by default even if the user does not belong to any group.
+    permission_classes = [DjangoModelPermissions]
+
+
     #! We've created a new series generator to display product details for the category when searching using the name filter at the category entry point:
     def get_serializer_class(self):
         if self.request.query_params.get('name'):
             return CategoryProductSerializer
         return super().get_serializer_class()
+
