9c05cc7952e7df79c9b7738cbd696599eca6e259
klc-pakize <pa3kize@gmail.com>
2023-02-28 14:39:39 +0300

    feat: added purchases urls

--- django_stock_app_2/stock/urls.py
+++ django_stock_app_2/stock/urls.py
@@ -2,13 +2,14 @@ from django.urls import path, include
 
 from rest_framework import routers
 
-from .views import CategoryView, BrandView, FirmView, ProductView
+from .views import CategoryView, BrandView, FirmView, ProductView, PurchasesView
 
 router = routers.DefaultRouter()
 router.register('categories', CategoryView)
 router.register('brands', BrandView)
 router.register('firms', FirmView)
 router.register('products', ProductView)
+router.register('purchases', PurchasesView)
 
 
 urlpatterns = [
