6c04a8578b1a80b3b00302fff6109209c57855ed
klc-pakize <pa3kize@gmail.com>
2023-02-28 14:19:16 +0300

    feat: added firm urls

--- django_stock_app_2/stock/urls.py
+++ django_stock_app_2/stock/urls.py
@@ -2,11 +2,12 @@ from django.urls import path, include
 
 from rest_framework import routers
 
-from .views import CategoryView, BrandView
+from .views import CategoryView, BrandView, FirmView
 
 router = routers.DefaultRouter()
 router.register('categories', CategoryView)
 router.register('brands', BrandView)
+router.register('firms', FirmView)
 
 
 urlpatterns = [
