60c81225d093a839cbe56571630cf601c0be45d7
klc-pakize <pa3kize@gmail.com>
2023-02-28 15:13:54 +0300

    feat: created sales serializer

--- django_stock_app_2/stock/serializers.py
+++ django_stock_app_2/stock/serializers.py
@@ -2,6 +2,8 @@ from rest_framework import serializers
 
 from .models import Category, Product, Brand, Firm, Purchases, Sales
 
+import datetime
+
 class CategorySerializer(serializers.ModelSerializer):
 
     product_count = serializers.SerializerMethodField()
@@ -60,6 +62,55 @@ class PurchasesSerializer(serializers.ModelSerializer):
     firm_id = serializers.IntegerField()
     brand_id = serializers.IntegerField()
     product_id = serializers.IntegerField()
+
+    time_hour = serializers.SerializerMethodField()
+    category = serializers.SerializerMethodField()
+    createds = serializers.SerializerMethodField()
     class Meta:
         model = Purchases
-        fields = ['id', 'user', 'user_id', 'firm', 'firm_id', 'brand', 'brand_id', 'product', 'product_id', 'quantity', 'price', 'price_total', 'createds', 'updated']
+        fields = ['id', 'user', 'user_id', 'firm', 'firm_id', 'brand', 'brand_id', 'product', 'product_id', 'quantity', 'price', 'price_total', 'time_hour', 'createds', 'category']
+
+    
+    def get_category(self, obj):
+        return obj.product.category.name
+    
+    def get_time_hour(self, obj):
+        return datetime.datetime.strftime(obj.createds, "%H:%M")
+    
+    def get_createds(self, obj):
+        return datetime.datetime.strftime(obj.createds, "%d,%m,%Y")
+    
+
+class SalesSerializer(serializers.ModelSerializer):
+    
+    user = serializers.StringRelatedField() 
+    brand = serializers.StringRelatedField()
+    product = serializers.StringRelatedField()
+    product_id = serializers.IntegerField()
+    brand_id = serializers.IntegerField()
+    time_hour = serializers.SerializerMethodField()
+    createds = serializers.SerializerMethodField()
+    
+    class Meta:
+        model = Sales
+        fields = (
+            "id",
+            "user",
+            "user_id",
+            "brand",
+            "brand_id",
+            "product",
+            "product_id",
+            "quantity",
+            "price",
+            "price_total",
+            "time_hour",
+            "createds",
+        )
+    
+    def get_time_hour(self, obj):
+        return datetime.datetime.strftime(obj.createds, "%H:%M")
+    
+    def get_createds(self, obj):
+        return datetime.datetime.strftime(obj.createds, "%d,%m,%Y")
+    
