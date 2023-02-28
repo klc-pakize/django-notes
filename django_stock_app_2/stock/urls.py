5fde5934582df2f8ecf6f3dcb9849cb0794cd2c7
klc-pakize <pa3kize@gmail.com>
2023-02-28 14:22:57 +0300

    feat: added product urls

--- django_stock_app_2/stock/urls.py
+++ django_stock_app_2/stock/urls.py
@@ -2,12 +2,13 @@ from django.urls import path, include
 
 from rest_framework import routers
 
-from .views import CategoryView, BrandView, FirmView
+from .views import CategoryView, BrandView, FirmView, ProductView
 
 router = routers.DefaultRouter()
 router.register('categories', CategoryView)
 router.register('brands', BrandView)
 router.register('firms', FirmView)
+router.register('products', ProductView)
 
 
 urlpatterns = [
